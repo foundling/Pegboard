0c7222fe55c858e84abffaf7b3382f84e397b5c6
Alex Ramsdell <alexramsdell@gmail.com>
2024-12-21 10:53:57 -0500

    create key for pegboard w/ initial set of symbols.

--- src/client/index.js
+++ src/client/index.js
@@ -1,55 +1,79 @@
-const templateGrid = document.querySelector('.template-grid');
-const templateGridSquares = templateGrid.querySelectorAll('.grid-square');
-const colorKeyGrid = document.querySelector('.color-key-grid');
-const allColorSquares = colorKeyGrid.querySelectorAll('.color-key-grid-square');
-const colorNames = ['white', 'red', 'blue', 'green', 'yellow'];
+function initPegboard() {
 
-let activeColor = null;
+  const templateGrid = document.querySelector('.template-grid');
+  const templateGridSquares = templateGrid.querySelectorAll('.grid-square');
+  const colorKeyGrid = document.querySelector('.color-key-grid');
+  const symbolKeyGrid = document.querySelector('.symbol-key-grid');
+  const symbolKeyGridSquares = symbolKeyGrid.querySelectorAll('.symbol-key-grid-square');
+  const allColorSquares = colorKeyGrid.querySelectorAll('.color-key-grid-square');
+  const colorNames = ['white', 'red', 'blue', 'green', 'yellow'];
 
-colorKeyGrid.addEventListener('click', (e) => {
-  if (!e.target.classList.contains('color-key-grid-square')) {
-    return;
-  }
-  activeColor = null;
-  allColorSquares.forEach(square => {
-    square.classList.remove('active');
+  const SYMBOLS = {
+    TRIANGLE: '&#9651;',
+    INVERTED_TRIANGLE: '&#x25BD;',
+    SQUARE: '&#x25A1;',
+    WHITE_CIRLCE: '&#x25CB;',
+    BLACK_CIRCLE: '&#x25CF;',
+  };
+
+  Object.entries(SYMBOLS).forEach(([name, unicodeValue], index) => {
+    const symbolKeyGridSquare = symbolKeyGridSquares[index];
+    symbolKeyGridSquare.innerHTML = unicodeValue;
+    //symbolKeyGridSquares[index].
   });
-  const colorId = e.target.id;
-  const colorSquare = document.querySelector(`.color-key-grid-square#${colorId}`)
-  colorSquare.classList.add('active');
-  activeColor = colorId;
 
-  console.log('active color: ', activeColor);
 
-});
+  let activeColor = null;
 
-templateGrid.addEventListener('click', (e) => {
-  console.log(e.target);
-
-  if (!e.target.classList.contains('grid-square')) {
-    return;
-  }
-
-  if (activeColor === null) {
-    return;
-  }
-
-  for (let i = 0; i < colorNames.length; ++i) {
-    const colorName = colorNames[i];
-
-    if (colorName !== activeColor && e.target.classList.contains(colorName)) {
-      e.target.classList.remove(colorName);
+  colorKeyGrid.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('color-key-grid-square')) {
+      return;
     }
-  }
+    activeColor = null;
+    allColorSquares.forEach(square => {
+      square.classList.remove('active');
+    });
+    const colorId = e.target.id;
+    const colorSquare = document.querySelector(`.color-key-grid-square#${colorId}`)
+    colorSquare.classList.add('active');
+    activeColor = colorId;
 
-  e.target.classList.toggle(activeColor);
+  });
+
+  templateGrid.addEventListener('click', (e) => {
+
+    if (!e.target.classList.contains('grid-square')) {
+      return;
+    }
+
+    // TODO: add some indication that you need to select a color.
+    if (activeColor === null) {
+      return;
+    }
+
+    // remove any existing color. TODO: refactor by matching against 'color-${colorName}'
+    for (let i = 0; i < colorNames.length; ++i) {
+      const colorName = colorNames[i];
+      const colorClassName = `color-${colorName}`;
+
+      if (colorName !== activeColor && e.target.classList.contains(colorClassName)) {
+        e.target.classList.remove(colorClassName);
+      }
+    }
+
+    // set new color
+    const activeColorClass = `color-${activeColor}`;
+    e.target.classList.toggle(activeColorClass);
 
 
-});
+  });
 
-// click on a key grid square => 
-// deactivates any other activated squares
-// activates that square
-// sets click color to that square's color
-const colorKeys = colorKeyGrid.querySelectorAll('.color-key-grid-square');
-console.log(colorKeys);
+  // click on a key grid square => 
+  // deactivates any other activated squares
+  // activates that square
+  // sets click color to that square's color
+  //const colorKeys = colorKeyGrid.querySelectorAll('.color-key-grid-square');
+
+}
+
+initPegboard();
